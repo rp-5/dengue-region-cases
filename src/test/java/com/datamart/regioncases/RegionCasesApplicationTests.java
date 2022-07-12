79e464a81cde97366506336595e49ab40d46ac3b
BhrunoB8 <bhruno.roan@yahoo.com>
2022-07-12 14:46:55 -0300

    testando integração continua

--- src/test/java/com/datamart/regioncases/RegionCasesApplicationTests.java
+++ src/test/java/com/datamart/regioncases/RegionCasesApplicationTests.java
@@ -2,12 +2,14 @@ package com.datamart.regioncases;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class RegionCasesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(true, "Test passing");
 	}
 
 }
